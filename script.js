c1bbb5fcbe2a40142c17d3afa0654da6e7280718
Elizabeth Lola <leezeelola@gmail.com>
2021-02-13 16:24:33 +0100

    converting to react

--- script.js
+++
@@ -1,9 +0,0 @@
-const showMenu = () => {
-    const nav = document.querySelector("#nav");
-
-    if (nav.className === "menu") {
-      nav.className += " responsive";
-    } else {
-      nav.className = "menu";
-    }
-  }
---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="h-screen flex justify-center items-center text-2xl font-medium">
+      I am working on something really amazing. Look out
+    </div>
+  );
+}
+
+export default App;
