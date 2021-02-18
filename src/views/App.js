a916599bbac0374fa851a57900347a865234cd97
Elizabeth Lola <leezeelola@gmail.com>
2021-02-18 10:35:21 +0100

    some changes

--- src/views/App.js
+++ src/views/App.js
@@ -8,6 +8,9 @@ const App = () => {
       <Navbar />
       <div className="flex h-96 justify-center items-center text-2xl font-medium">
         I am working on something really amazing. Look out
+        <a href="https://dev.to/ilizette">
+          <img src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt="Elizabeth's DEV Community Profile" height="30" width="30" />
+        </a>
       </div>
       <Footer />
     </div>
--- src/views/Footer/index.js
+++ src/views/Footer/index.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Footer = () => {
 
     return (
-        <footer className="fixed bottom-0 z-50 bg-dark shadow-lg w-full px-3 py-5 grid grid-cols-3 items-center justify-items-center">
-            <a href="/" className="inline-block px-7 text-xl">Projects</a>
-            <a href="/" className="inline-block px-7 text-xl">Writings</a>
-            <a href="/" className="inline-block px-7 text-xl">Contact</a>
+        <footer className="fixed bottom-0 z-50 bg-dark shadow-lg w-full px-3 py-5 grid grid-cols-3 items-center justify-items-center md:hidden">
+            <a href="/" className="inline-block px-7 sm:text-sm text-xs">Projects</a>
+            <a href="/" className="inline-block px-7 sm:text-sm text-xs">Writings</a>
+            <a href="/" className="inline-block px-7 sm:text-sm text-xs">Contact</a>
         </footer>
     )
 
