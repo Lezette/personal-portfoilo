cfacbae7edd267b48e3ca21522af0ea7c641a58f
Elizabeth Lola <leezeelola@gmail.com>
2021-02-26 16:59:06 +0100

    intro

--- src/views/Hero/index.jsx
+++ src/views/Hero/index.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 const Hero = () => {
 
     return (
-        <section>
-            Hero section
+        <section className="grid md:grid-cols-2">
+            <div>Hello I am Elizabeth an I do Frontend and UX designs</div>
+            <div>Sure</div>
         </section>
     )
 
